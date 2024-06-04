37944e5323383abc20a8d491786326e70c859fc9
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-06-04 09:51:55 +0300

    added User model

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+export const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    avatarURL: String,
+  },
+  { versionKey: false }
+);
