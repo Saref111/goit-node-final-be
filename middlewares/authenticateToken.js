45705bcc09929aa5440886a3ab44087d58e3f21d
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-06-04 21:13:20 +0300

    removed unused code

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -1,10 +1,8 @@
 import passport from "passport";
-import usersServices from "../services/usersServices.js";
-import { getTokenfromReq } from "./getTokenfromReq.js";
 
 export const authenticateToken = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, async (err, user) => {
-    if (err || !user || await usersServices.isTokenBlacklisted(getTokenfromReq(req))) {
+    if (err || !user) {
       return res.status(401).json({ message: "Not authorized" });
     }
     req.user = user;
