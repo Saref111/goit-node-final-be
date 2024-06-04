cf4db1c87e0a524f09deb0c6b9ea5e099ae87489
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-06-04 09:47:23 +0300

    added main app layer

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+// import usersRouter from "./routes/usersRouter.js";
+import './config/config-passport.js';
+
+dotenv.config();
+
+const uri = process.env.DB_HOST;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// app.use("/api/users", usersRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+});
