17bd6703f6e5841d34588bc1ccc22ddf5d1dbd8c
Yulia <nayanzina@gmail.com>
2024-06-05 20:16:24 +0300

    add userController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,7 @@
 import * as userService from "../services/userService.js";
+import User from "../models/User.js";
+import HttpError from "../helpers/HttpError.js";
+import { ctrlWrapper } from "../decorators/index.js";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -9,6 +12,122 @@ export const register = async (req, res) => {
   }
 
   const user = await userService.createUser({ name, email, password });
-  
+
   res.status(201).json({ user });
+};
+
+const getAll = async (req, res) => {
+  // const { _id: owner } = req.user;
+  // const { page = 1, limit = 10 } = req.query;
+  // const skip = (page - 1) * limit;
+  const result = await User.find();
+  //  ( { owner }
+  //   //   , "-createdAt -updatedAt", {
+  //   //   skip,
+  //   //   limit,
+  //   // }
+  // ).populate("owner", "name email");)
+  res.json(result);
+};
+
+const getFollowersById = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  if (!user) {
+    throw HttpError(404, `User with id=${id} not found`);
+  }
+  const result = user.followers;
+  res.json(result);
+};
+
+const getFollowings = async (req, res) => {
+  // змінити коли буде авторизований user -->
+  // ----------------------------------------------------
+  // const { _id: userId } = req.user;
+  const { id: userId } = req.params;
+  // ----------------------------------------------------
+  const user = await User.findById(userId);
+  if (!user) {
+    throw HttpError(404, `User with id=${userId} not found`);
+  }
+  const result = user.following;
+  res.json(result);
+};
+
+const addFollowing = async (req, res) => {
+  const { id } = req.params;
+  const currentUserId = "64c8d958249fae54bae90bb9";
+
+  const followedUser = await User.findById(id);
+  if (!followedUser) {
+    throw HttpError(404, `User with id=${id} not found`);
+  }
+  const { followers } = followedUser;
+  followers.push(currentUserId);
+
+  const { following } = await User.findById(currentUserId);
+  if (following.includes(id)) {
+    throw HttpError(404, `You are already followed to a user with id=${id}`);
+  }
+  following.push(id);
+  const result = await User.findByIdAndUpdate(
+    currentUserId,
+    { following: following },
+    {
+      new: true,
+    }
+  );
+
+  const followedUserUpdate = await User.findByIdAndUpdate(
+    id,
+    { followers: followers },
+    {
+      new: true,
+    }
+  );
+
+  res.json(result.following);
+};
+
+const removeFollowing = async (req, res) => {
+  const { id } = req.params;
+  const currentUserId = "64c8d958249fae54bae90bb9";
+
+  const followedUser = await User.findById(id);
+  if (!followedUser) {
+    throw HttpError(404, `User with id=${id} not found`);
+  }
+  const { followers } = followedUser;
+  const newFollowers = followers.filter(
+    (follower) => follower !== currentUserId
+  );
+
+  const { following } = await User.findById(currentUserId);
+  const newFollowings = following.filter((followingId) => followingId !== id);
+  const result = await User.findByIdAndUpdate(
+    currentUserId,
+    { following: newFollowings },
+    {
+      new: true,
+    }
+  );
+
+  const followedUserUpdate = await User.findByIdAndUpdate(
+    id,
+    { followers: newFollowers },
+    {
+      new: true,
+    }
+  );
+
+  res.json(result.following);
+};
+
+export default {
+  register: ctrlWrapper(register),
+  getAll: ctrlWrapper(getAll),
+  getFollowersById: ctrlWrapper(getFollowersById),
+  getFollowings: ctrlWrapper(getFollowings),
+  addFollowing: ctrlWrapper(addFollowing),
+  removeFollowing: ctrlWrapper(removeFollowing),
 };
--- middlewares/validateId.js
+++ middlewares/validateId.js
@@ -1,12 +1,12 @@
-import {isValidObjectId} from "mongoose"; 
+import { isValidObjectId } from "mongoose";
 import HttpError from "./HttpError.js";
 
 const validateId = (req, res, next) => {
-    const {id} = req.params;
-    if(!isValidObjectId(id)) {
-        next(HttpError(400, `${id} is not valid id`))
-    }
-    next();
-}
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
+    next(HttpError(400, `${id} is not valid id`));
+  }
+  next();
+};
 
 export default validateId;
