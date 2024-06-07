0ae00143f4f1d5cdb6d86b17e8e77a98e5cbea9f
filimon4uck <filimon4uck.yura@gmail.com>
2024-06-07 12:45:13 +0200

    added query scheme added validate query, added countDocuments, added recipes router, added recipes route, added recipe router added recipe route, added recipes service, added recipe service

--- routes/recipesRouter.js
+++ routes/recipesRouter.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import recipesControllers from "../controllers/recipesController.js";
 import { authenticateToken } from "../middlewares/index.js";
+import validateQuery from "../helpers/validateQuary.js";
+import { querySchema } from "../schemas/querySchema.js";
 
 const recipesRouter = Router();
-
+recipesRouter.get(
+  "/",
+  validateQuery(querySchema),
+  recipesControllers.getQueryRecipes
+);
+recipesRouter.get("/:id", recipesControllers.getOneRecipe);
 recipesRouter.get("/popular", recipesControllers.getPopular);
 
 recipesRouter.use(authenticateToken);
