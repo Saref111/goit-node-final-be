1f42e20b16f486017e8177beb9b341426bdba80f
filimon4uck <filimon4uck.yura@gmail.com>
2024-06-12 23:40:06 +0200

    fixed recipe controller, recipe service

--- services/recipesService.js
+++ services/recipesService.js
@@ -9,7 +9,7 @@ export const getRecipe = (filter) => {
   return Recipe.findOne(filter)
     .populate("owner", "name avatar")
     .populate("ingredients.id")
-    .populate("category")
+    .populate("category", "-thumb -description")
     .populate("area");
 };
 
